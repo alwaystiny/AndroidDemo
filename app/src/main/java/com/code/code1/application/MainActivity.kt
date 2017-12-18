a46384b0add8fe70d13dee46c2db5989a3bbcd8a
cdk <cdk0718>
2017-12-18 14:45:19 +0800

    // 下拉zj代码 处理冲突

--- app/src/main/java/com/code/code1/application/MainActivity.kt
+++ app/src/main/java/com/code/code1/application/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         init()
         initViews()
     }
@@ -19,12 +18,13 @@ class MainActivity : AppCompatActivity() {
         Log.i("test2","test222222")
         Log.i("test3","test3333333333333333")
         Log.i("test4","test4444444444444")
-        initViews();
     }
 
     private fun initViews() {
         Log.e("cdk say" , "zjsb")
         Log.e("cdk say again" , "zjsb")
         Log.e("cdk say again again" , "zjsb")
+        Log.e("cdk say again again" , "zjsb absolutely")
+
     }
 }
