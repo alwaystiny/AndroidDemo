4f77f93c4ebc0f31c1556dbb7e0500ba80632209
cdk <cdk0718>
2017-12-18 13:43:46 +0800

    //

--- app/src/main/java/com/code/code1/application/MainActivity.kt
+++ app/src/main/java/com/code/code1/application/MainActivity.kt
@@ -2,11 +2,17 @@ package com.code.code1.application
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews();
+    }
+
+    private fun initViews() {
+        Log.e("cdk say" , "zjsb")
     }
 }
